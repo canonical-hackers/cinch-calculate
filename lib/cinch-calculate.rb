32acb6b1b24a7928cc56a7ad6fe8f58269ec6763
Brian Haberer <bhaberer@gmail.com>
2013-05-06 15:25:59 -0700

    first commit

---
+++ lib/cinch-calculate.rb
@@ -0,0 +1,7 @@
+require "cinch-calculate/version"
+
+module Cinch
+  module Calculate
+    # Your code goes here...
+  end
+end
---
+++ lib/cinch-calculate/version.rb
@@ -0,0 +1,5 @@
+module Cinch
+  module Calculate
+    VERSION = "0.0.1"
+  end
+end
