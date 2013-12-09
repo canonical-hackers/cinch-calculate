11cd3a4447ebe3b383e09405339ae117364b8568
Brian Haberer <bhaberer@gmail.com>
2013-12-09 13:35:06 -0800

    Style Guide Adherance, v1.0.2

--- lib/cinch/plugins/calculate.rb
+++ lib/cinch/plugins/calculate.rb
@@ -1,10 +1,13 @@
+# -*- encoding : utf-8 -*-
 require 'cinch'
 
 module Cinch::Plugins
+  # Cinch plugin to allow complex math calculations to be done.
   class Calculate
     include Cinch::Plugin
 
-    self.help = "Use .math <problem> to do math computations. (e.g. 5 feet / inches, .math sin(2) + 4)"
+    self.help = 'Use .math <problem> to do math computations.' +
+                '(e.g. 5 feet / inches, .math sin(2) + 4)'
 
     match /math (.+)/
 
@@ -21,14 +24,15 @@ module Cinch::Plugins
 
     def math(problem_string)
       return 'Sorry, I can\'t do that' unless units_binary_exists?
-      return IO.popen([@units_path, "-t", problem_string]).readline.chomp
+      return IO.popen([@units_path, '-t', problem_string]).readline.chomp
     rescue EOFError
-      debug "Make sure you have GNU Units installed and not the BSD Units that ships with OSX."
+      debug 'Make sure you have GNU Units installed ' +
+            'and not the BSD Units that ships with OSX.'
     end
 
     def units_binary_exists?
       return true if File.exist? @units_path
-      debug "Cinch can't find the unit conversion binary."
+      debug 'Cinch can\'t find the unit conversion binary.'
       false
     end
   end
