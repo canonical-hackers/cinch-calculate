5fa8ed47eb5ccd06e62b82ce2c2d4f3e57bdc12d
Brian Haberer <bhaberer@gmail.com>
2014-07-29 11:28:11 -0700

    Small style fixes

--- lib/cinch/plugins/calculate.rb
+++ lib/cinch/plugins/calculate.rb
@@ -2,41 +2,43 @@
 require 'cinch'
 require 'cinch/cooldown'
 
-module Cinch::Plugins
-  # Cinch plugin to allow complex math calculations to be done.
-  class Calculate
-    include Cinch::Plugin
+module Cinch
+  module Plugins
+    # Cinch plugin to allow complex math calculations to be done.
+    class Calculate
+      include Cinch::Plugin
 
-    self.help = 'Use .math <problem> to do math computations.' +
-                '(e.g. 5 feet / inches, .math sin(2) + 4)'
+      self.help = 'Use .math <problem> to do math computations.' \
+                  '(e.g. 5 feet / inches, .math sin(2) + 4)'
 
-    enforce_cooldown
+      enforce_cooldown
 
-    match /math (.+)/
+      match(/math\s(.+)/)
 
-    def initialize(*args)
-      super
-      @units_path = config[:units_path] || '/usr/bin/units'
-    end
+      def initialize(*args)
+        super
+        @units_path = config[:units_path] || '/usr/bin/units'
+      end
 
-    def execute(m, problem)
-      m.reply math(problem), true
-    end
+      def execute(m, problem)
+        m.reply math(problem), true
+      end
 
-    private
+      private
 
-    def math(problem_string)
-      return 'Sorry, I can\'t do that' unless units_binary_exists?
-      return IO.popen([@units_path, "-t", problem_string]).readline.chomp
-    rescue EOFError
-      debug 'Make sure you have GNU Units installed ' +
-            'and not the BSD Units that ships with OSX.'
-    end
+      def math(problem_string)
+        return 'Sorry, I can\'t do that' unless units_binary_exists?
+        return IO.popen([@units_path, '-t', problem_string]).readline.chomp
+      rescue EOFError
+        debug 'Make sure you have GNU Units installed ' \
+              'and not the BSD Units that ships with OSX.'
+      end
 
-    def units_binary_exists?
-      return true if File.exist? @units_path
-      debug 'Cinch can\'t find the unit conversion binary.'
-      false
+      def units_binary_exists?
+        return true if File.exist? @units_path
+        debug 'Cinch can\'t find the unit conversion binary.'
+        false
+      end
     end
   end
 end
